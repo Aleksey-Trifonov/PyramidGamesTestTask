f13d561bf1911653751bb57732a2ce9f3437d0f8
Aleksey-Trifonov <altrishka@gmail.com>
2021-02-24 22:09:58 +0100

    Scene composition and preliminary scripting

---
+++ Test_task/Assets/Scripts/GameSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameSettings", menuName = "ScriptableObjects/GameSettingsScriptableObject", order = 1)]
+public class GameSettings : ScriptableObject
+{
+    public float GolfHoleXOffset = 1.5f;
+    public float GolfBallXVelocityIncrementRate = 1.2f;
+    public float GolfBallYVelocityIncrementRate = 1f;
+    public float ConsecutiveWinVelocityModifier = 0.1f;
+}
